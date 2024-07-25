b2de08cd9093d0a3352a01bb07ab8b9dca6e0c60
Muhammad Shahzaib Siddiqui <s.zaib356@gmail.com>
2024-07-25 15:57:00 +0500

    Analog Clock

---
+++ js.js
@@ -0,0 +1,12 @@
+function startClock() {
+    let hourBox = document.querySelector('#hour');
+    let minBox  = document.querySelector('#min');
+    let secBox  = document.querySelector('#sec');
+
+    secBox.style.transform = "";
+    console.log("🚀 ~ startClock ~ secBox:", )
+
+
+}
+
+setInterval('startClock()', 1000)
